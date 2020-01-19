c96984e816ac442e1030d69d78a05b484ec98fc6
MarvinHo1 <47537343+MarvinHo1@users.noreply.github.com>
2020-01-19 01:48:22 -0800

    Update api.js

--- MVP/database/apiKey/api.js
+++ MVP/database/apiKey/api.js
@@ -1,5 +1,3 @@
-const apiKey = '234e3653817f546f2cecdbc8b6e93c98';
-const GalleryId = '183286107-72157710226002996';
 
 const flickrPhotos = `https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=${apiKey}&gallery_id=${GalleryId}&format=json&nojsoncallback=1`;
 
